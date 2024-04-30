9a1ed1ec9805f5264a1a976a4b2c87a9a69e1689
jinjooyoung <jjy26297449@gmail.com>
2024-04-30 16:22:51 +0900

    9주차 시험범위 복습 테스트

---
+++ Assets/Scripts/Song_Class_Homework/8week/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log('가' + "나다라");
+        Debug.Log('가' + '나');
+        Debug.Log(2 + 3);
+        Debug.Log(2 + 3.2);
+        Debug.Log('가' * 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
