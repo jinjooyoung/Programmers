d6244e2867fc3ca9b2bc67a1dc54dfa864a30684
jinjooyoung <jjy26297449@gmail.com>
2024-04-30 15:00:35 +0900

    9주차

--- Assets/Scripts/Song_Class_Homework/8week/Four_Ten.cs
+++ Assets/Scripts/Song_Class_Homework/8week/Four_Ten.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Four_Nine : MonoBehaviour
+public class Four_Ten : MonoBehaviour
 {
     // 프로그래밍의 이해 10주차 강의자료
-    // 기본예제 4-9
-    // foreach 반복문과 배열
+    // 기본예제 4-10
+    // foreach 반복문과 var키워드
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,8 @@ public class Four_Nine : MonoBehaviour
         string[] array = { "사과", "배", "포도", "딸기", "바나나" };
 
         // 반복을 수행합니다.
-        foreach (string item in array)
+        // foreach (string item in array)
+        foreach (var item in array)
         {
             //출력합니다.
             Debug.Log(item);
