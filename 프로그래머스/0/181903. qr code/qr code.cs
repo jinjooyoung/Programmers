7aa5dc9decd5d9b5247b990d87242ac807a137e8
jinjooyoung <163229483+jinjooyoung@users.noreply.github.com>
2024-07-15 17:31:29 +0900

    [level 0] Title: qr code, Time: 1.26 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public string solution(int q, int r, string code) {
+        string answer = "";
+        for (int i = 0; i < code.Length; i++)
+        {
+            if (i%q == r)
+            {
+                answer += code[i];
+            }
+        }
+        return answer;
+    }
+}
