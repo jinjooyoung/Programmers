3518a95ef00a2af55a90bb99d6ef0e83670f06a2
jinjooyoung <163229483+jinjooyoung@users.noreply.github.com>
2024-07-23 09:50:48 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.66 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] intervals) {
+        int resultLength = intervals[0,1] - intervals[0,0] + intervals[1,1] - intervals[1,0] + 2;
+        int[] answer = new int[resultLength];
+        int index = 0;
+        
+        for (int i = intervals[0,0]; i <= intervals[0,1]; i++)
+        {
+            answer[index] = arr[i];
+            index++;
+        }
+        for (int i = intervals[1,0]; i <= intervals[1,1]; i++)
+        {
+            answer[index] = arr[i];
+            index++;
+        }
+        
+        return answer;
+    }
+}
