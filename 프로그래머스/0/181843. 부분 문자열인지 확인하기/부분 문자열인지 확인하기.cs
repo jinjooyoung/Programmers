1a9dff21b5e9e69bb5581ff8ca85b770bf1ee793
jinjooyoung <163229483+jinjooyoung@users.noreply.github.com>
2024-11-14 20:39:26 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.31 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string, string target) {
+        if (my_string.Contains(target))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
