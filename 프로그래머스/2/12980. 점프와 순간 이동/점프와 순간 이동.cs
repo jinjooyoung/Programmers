ade99c4d0dae0a795e6ccb702f37eabac2499070
jinjooyoung <163229483+jinjooyoung@users.noreply.github.com>
2026-03-09 21:50:17 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+
+        while(n > 0)
+        {
+            if(n%2!=0)  answer++;
+            n/=2;
+        }
+
+        return answer;
+    }
+}
